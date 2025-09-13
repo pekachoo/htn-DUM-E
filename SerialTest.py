18cd3c046c868b11c99e6132004291fe2b74dc34
Jason Liu <jliu6162@gmail.com>
2025-09-13 02:35:53 -0400

    basic serial test

--- SerialTest.py
+++ SerialTest.py
@@ -5,7 +5,6 @@ ser = serial.Serial("/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0",  9600, t
 time.sleep(2)
 
 while True:
-    ser.write("ON".encode())
+    ser.write("yaw:30;p1:15;p2:30;p3:45;roll:30;cw:40")
     time.sleep(2)
-    ser.write("OFF".encode())
-    time.sleep(2)
+    ser.write("yaw:30;p1:15;p2:30;p3:45;roll:30;cw:40")
