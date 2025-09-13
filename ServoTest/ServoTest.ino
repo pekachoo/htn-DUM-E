058b5d60b7213e49bb6194e27332f21b1fb4d0d0
pekachoo <jliu6162@gmail.com>
2025-09-13 00:55:46 -0400

    Create ServoTest.ino

---
+++ ServoTest/ServoTest.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(9);  // Connect the servo signal wire to pin 9
+}
+
+void loop() {
+  // Move from 0 to 180 degrees
+  // for (int pos = 0; pos <= 180; pos++) {
+  //   myServo.write(pos);
+  //   delay(15);           // small pause so the servo can reach the position
+  // }
+  myServo.write(0);
+
+  delay(5000);  // wait half a second
+
+  myServo.write(360);
+
+  // Move back to 0 degrees
+  // for (int pos = 180; pos >= 0; pos--) {
+  //   myServo.write(pos);
+  //   delay(15);
+  // }
+
+  delay(500);
+}
