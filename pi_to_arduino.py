aad3c56c281dba465529d37d03b2dac63a0d6bc0
Jason Liu <jliu6162@gmail.com>
2025-09-13 19:53:03 -0400

    yub

--- pi_to_arduino.py
+++ pi_to_arduino.py
@@ -47,7 +47,7 @@ class IKSolver:
 def IK_to_servo_angles(angles):
     yaw, p1, p2, p3, roll, claw = angles
     # yaw
-    yaw = -0.6556 * yaw + 110.0
+    yaw = -0.6556 * yaw + 105.0
     # p1
         ## restrain p1 angle
     if p1 > -90 and p1 < -35:
@@ -71,7 +71,7 @@ def sendTargets(angles, uno):
     yaw, p1, p2, p3, roll, claw = angles
 
     uno_string = f"yaw:{yaw:.2f};p1:{p1:.2f};p2:{p2:.2f};p3:{p3:.2f};roll:{roll:.2f};cw:{claw:.2f}\n"
-
+    print(uno_string)
     uno.write(uno_string.encode())
 
 def get_yaw_angle(x, y):
@@ -93,7 +93,7 @@ def grab(ikSolver, x, y, phi, ser):
         time.sleep(0.3)  # wait for arm to reach position
         
         #grab
-        down_angle = ikSolver.solve(math.sqrt(x**2 + y**2), -5, 270 * math.pi / 180, elbow='up')
+        down_angle = ikSolver.solve(math.sqrt(x**2 + y**2), -5, phi * math.pi / 180, elbow='up')
         angles = (get_yaw_angle(x, y),) + down_angle + (0, 1)  # open claw
         angles = tuple(int(math.degrees(a)) for a in angles)  # deg
         sendTargets(angles, ser)
@@ -110,7 +110,7 @@ def grab(ikSolver, x, y, phi, ser):
         time.sleep(0.3)  # wait for arm to reach position
         break
 
-def move_to_idle_position(ikSolver):
+def move_to_idle_position(ikSolver, ser):
     t = time.time()
     while(time.time() - t <= 1.5):
         # SETTING TARGETS
@@ -128,34 +128,38 @@ def move_to_idle_position(ikSolver):
         sendTargets(angles, ser)
         print("a")
 
-        time.sleep(0.3)  # Control update rate
+        time.sleep(0.1)  # Control update rate
+
+def move(ikSolver, x, y, z, phi, ser, claw_open=1, roll_angle=0, time_duration=2, elbow='up'):
+    t = time.time()
+    while(time.time() - t <= time_duration):
+        # Calculate angles
+        x_target, y_target = projection(x, y, z)
+        pitch_angles = ikSolver.solve(x_target, y_target, phi, elbow=elbow)
+        angles = (get_yaw_angle(x, y),) + pitch_angles + (roll_angle, claw_open)
+        angles = tuple(int(math.degrees(a)) for a in angles)  # convert to degrees
+        print(angles)
+        print("b")
+        sendTargets(angles, ser)
+
+def wave_bye(ikSolver, ser):
+    """Go to idle position, open claw, wave by oscillating L2 6 times"""
+    move_to_idle_position(ikSolver, ser)
+    time.sleep(2)
+    for _ in range(3):
+        move(ikSolver, -4, 0, 22, 135 * math.pi / 180, ser, claw_open=1, time_duration=1.5)
+        move(ikSolver, 4, 0, 22, 45 * math.pi / 180, ser, claw_open=1, time_duration=1.5)
 
 
 if __name__ == "__main__":
-    ser = serial.Serial("/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0",  9600, timeout=1)
+    ser = serial.Serial("/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0",  9600, timeout=5)
     ikSolver = IKSolver(P1=12.0, P2=12.3, P3=8.0)
+    time.sleep(2)
 
     try:
-        #while True:
-            # # SETTING TARGETS
-            # x, y, z = 4 * math.sqrt(2), 4 * math.sqrt(2), 22  # cm
-            # phi = 0 * math.pi / 180
-            # roll_angle, claw_open = 0, 1 
-
-            # # Calculate angles
-            # x_target, y_target = projection(x, y, z)
-            # pitch_angles = ikSolver.solve(x_target, y_target, phi, elbow='up')
-            # angles = (get_yaw_angle(x, y),) + pitch_angles + (roll_angle, claw_open)
-            # angles = tuple(int(math.degrees(a)) for a in angles)  # convert to degrees
-            # # sendTargets(angles, uno, master)
-            # print(angles)
-            # physical_angles = IK_to_servo_angles(angles)
-            # print(physical_angles)
-            # sendTargets(angles, ser)
-
-            # time.sleep(0.5)  # Control update rate
-            move_to_idle_position(ikSolver)
-            grab(ikSolver, 15, 15, 270, ser)
+        # wave_bye(ikSolver, ser)
+        move_to_idle_position(ikSolver, ser)
+        # grab(ikSolver, 15, 15, 270, ser)
 
     except KeyboardInterrupt:
         print("Stopped by user.")
